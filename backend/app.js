aa86dd3c5cb4a7f84ef863f8f342c07d2f47bee7
inesbdl <bouadilin@gmail.com>
2025-06-25 15:47:51 +0200

    test eslint ci

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const { db } = require("./models/db");
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./models/db');
 
-const bookRouter = require("./routes/bookRoute");
-const authorRouter = require("./routes/authorRoute");
-const userRouter = require("./routes/userRoute");
-const editionRouter = require("./routes/editionRoute");
-const themeRouter = require("./routes/themeRoute");
-const typeRouter = require("./routes/typeRoute");
+const bookRouter = require('./routes/bookRoute');
+const authorRouter = require('./routes/authorRoute');
+const userRouter = require('./routes/userRoute');
+const editionRouter = require('./routes/editionRoute');
+const themeRouter = require('./routes/themeRoute');
+const typeRouter = require('./routes/typeRoute');
 
 const app = express();
 const PORT = process.env.PORT_BACK || 2000;
@@ -17,18 +17,18 @@ app.use(cors({
     allowedHeaders: ['Content-Type'],
 }));
 
-app.use(express.json({limit: "2mb"}));
+app.use(express.json({limit: '2mb'}));
 
-app.use("/api/v1/books",bookRouter);
-app.use("/api/v1/authors",authorRouter);
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/editions",editionRouter);
-app.use("/api/v1/themes",themeRouter);
-app.use("/api/v1/types",typeRouter);
+app.use('/api/v1/books',bookRouter);
+app.use('/api/v1/authors',authorRouter);
+app.use('/api/v1/users',userRouter);
+app.use('/api/v1/editions',editionRouter);
+app.use('/api/v1/themes',themeRouter);
+app.use('/api/v1/types',typeRouter);
 
 db.sync(/*{force : true}*/)
     .then(async () => {
             app.listen(PORT, () => {
             console.log(`http://localhost:${PORT}`);
-        })
+        });
     });
---
+++ readme-tp3.md
@@ -0,0 +1,16 @@
+
+# Partie 1
+
+`npx eslint backend/app.js`
+
+```
+  24:9   error  Strings must use singlequote  quotes
+  25:9   error  Strings must use singlequote  quotes
+  25:9   error  Strings must use singlequote  quotes
+  26:9   error  Strings must use singlequote  quotes
+  26:9   error  Strings must use singlequote  quotes
+  27:9   error  Strings must use singlequote  quotes
+  33:11  error  Missing semicolon             semicolon
+```
+
+# Partie 2
